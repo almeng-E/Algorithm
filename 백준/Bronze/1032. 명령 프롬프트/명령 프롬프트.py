bb9c44f3e5b51333f6e1bd121074fbdb328a6fff
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-02-10 23:05:16 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -0,0 +1,22 @@
+N = int(input())
+
+words = []
+for _ in range(N):
+    words.append(input())
+
+word_length = len(words[0])
+
+res = [0 for _ in range(word_length)]
+
+
+for j in range(word_length):
+    tmp = words[0][j]
+    for i in range(N):
+        if words[i][j] != tmp:
+            res[j] = '?'
+            break
+        else:
+            res[j] = tmp
+
+output = ''.join(res)
+print(output)
