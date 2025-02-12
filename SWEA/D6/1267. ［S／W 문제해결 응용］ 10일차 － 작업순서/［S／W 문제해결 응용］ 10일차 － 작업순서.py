5c371cbde954e1940b8f469255dc5422f5ffbc7e
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-02-13 01:58:07 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 158 ms, Memory: 75,648 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
@@ -1,20 +1,20 @@
+# 조금 더 느린  ?? 코드 ??
+# must_visited 탐색 회수가 훨씬 많아 더 느릴 것으로 예상됨
+
+
 def dfs(graph, visited, current_node):
     global must_precede, res
-    # 방문 처리 전, 체크하기
-    if must_precede[current_node]:
-        return
+    # 방문 처리 전, 체크하기    <<< 차이점
+    for i in must_precede[current_node]:
+        if not visited[i]:
+            return
     # 방문 및 재귀 호출
-    else:
-        res.append(current_node)
-        visited[current_node] = 1
-        # 선행 처리
-        for i in must_precede:
-            if current_node in i:
-                i.remove(current_node)
-        # 재귀
-        for next_node in graph[current_node]:
-            if not visited[next_node]:
-                dfs(graph, visited, next_node)
+    res.append(current_node)
+    visited[current_node] = 1
+    # 재귀
+    for next_node in graph[current_node]:
+        if not visited[next_node]:
+            dfs(graph, visited, next_node)
 
 
 
