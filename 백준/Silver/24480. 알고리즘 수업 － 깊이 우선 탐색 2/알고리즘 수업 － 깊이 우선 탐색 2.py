47ec367f24bd9ba0bd3eabb6162826ad7c688082
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-02-13 00:53:04 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 624 ms, Memory: 60832 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+
+
+def dfs_stack(graph, start):
+    visited = [0 for _ in range(N + 1)]
+    stack = [start]
+    cnt = 0
+    while stack:
+        v = stack.pop()
+        if not visited[v]:
+            cnt += 1
+            visited[v] = cnt
+            for next_node in graph[v]:
+                if not visited[next_node]:
+                    stack.append(next_node)
+
+    return visited
+
+N, M, R = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+
+# 그래프 정보 저장 (양방향)
+for _ in range(M):
+    U, V = map(int, input().split())
+    graph[U].append(V)
+    graph[V].append(U)
+
+# 그래프 내부 정렬
+for i in range(1, N+1):
+    graph[i].sort()
+
+# dfs 실행
+visited = dfs_stack(graph, R)
+
+
+# 출력
+for i in range(1, N+1):
+    print(visited[i])
