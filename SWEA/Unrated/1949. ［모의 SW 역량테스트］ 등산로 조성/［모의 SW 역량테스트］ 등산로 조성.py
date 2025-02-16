ed81282d13ab9d9fe556fe99c3f5e71593ce854c
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-02-16 18:35:44 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 744 ms, Memory: 81,536 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
@@ -1,75 +1,71 @@
-from copy import deepcopy
-from collections import deque
+
+def dfs(i, j, can_dig, tmp):
+    # board, visited, steps, K
+    global res, K
+    tmp += 1
+    if res < tmp:
+        res = tmp
+
+    current_height = board[i][j]
+    for dx, dy in steps:
+        nx = i + dx
+        ny = j + dy
+        # 조기 종료 조건 설정
+        if nx < 0 or nx >= N or ny < 0 or ny >= N: continue
+        if visited[nx][ny]: continue
+
+        next_height = board[nx][ny]
+
+        # 기존 높이보다 크거나 같으면 ==> can dig 확인...
+        if next_height >= current_height and can_dig and next_height - current_height < K:
+            # 땅파고 dfs
+            # 들어갈 때 문 열고
+            can_dig = False
+            board[nx][ny] = current_height - 1
+            visited[nx][ny] = True
+            dfs(nx, ny, can_dig, tmp)
+            # 나올 때 문 닫기
+            visited[nx][ny] = False
+            board[nx][ny] = next_height
+            can_dig = True
+        if next_height < current_height:
+            # 모든 조건에 맞으면 GO !
+            # 들어갈 때 문 열고
+            visited[nx][ny] = True
+            dfs(nx, ny, can_dig, tmp)
+            # 나올 때 문 닫기
+            visited[nx][ny] = False
 
 
-def bfs(x, y, dig_i=-1, dig_j=-1, dig=0, copy=False):
-    if x == dig_i and y == dig_j:
-        return 0
-    tmp_board = [[1 for _ in range(N)] for _ in range(N)]
-    if copy:
-        board = deepcopy(original_board)
-        board[dig_i][dig_j] -= dig
-    else:
-        board = original_board
-    queue = deque()
-    queue.append((x, y))
-    while queue:
-        i, j = queue.popleft()
-        current_height = board[i][j]
-        for dx, dy in steps:
-            nx = i + dx
-            ny = j + dy
-            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] < current_height:
-                if tmp_board[nx][ny] <= tmp_board[i][j] + 1:
-                    tmp_board[nx][ny] = tmp_board[i][j] + 1
-                queue.append((nx, ny))
-    # 최장 길이 찾기
-    tmp = find_max(tmp_board)
-    return tmp
 
-
-def find_max(arr):
-    tmp = 0
-    for i in arr:
-        max_val = max(i)
-        if tmp < max_val:
-            tmp = max_val
-    return tmp
-
-
-#           상       우        하      좌
+#           상       우       하       좌
 steps = ((-1, 0), (0, +1), (+1, 0), (0, -1))
 
 T = int(input())
+
 for TC in range(T):
     N, K = map(int, input().split())
 
-    result = 0
+    board = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[False for _ in range(N)] for _ in range(N)]
 
-    original_board = [list(map(int, input().split())) for _ in range(N)]
 
-    # 최고 봉우리 찾기
-    max_height = find_max(original_board)
-
-    # 최고 봉우리의 위치 찾기
+    # 가장 높은 봉우리 찾기
     peaks = []
+    highest_h = max(map(max, board))
     for i in range(N):
         for j in range(N):
-            if original_board[i][j] == max_height:
+            if board[i][j] == highest_h:
                 peaks.append((i, j))
 
-    # 각 봉우리에서 시작하여 DFS로 최대 길이 찾고 result 와 비교
-    for x, y in peaks:
-        tmp = bfs(x, y)
-        if result < tmp:
-            result = tmp
 
-    # 땅 파고 다시 계산
-    for i in range(N):
-        for j in range(N):
-            for k in range(1, K+1):
-                for (x, y) in peaks:
-                    tmp = bfs(x, y, dig_i=i, dig_j=j, dig=k, copy=True)
-                    if result < tmp:
-                        result = tmp
-    print(f'#{TC+1} {result}')
+    # 실행
+    res = 0
+    for i, j in peaks:
+        # 방문 처리 (갇힘 현상 방지)
+        visited[i][j] = True
+        dfs(i, j, True, 0)
+        visited[i][j] = False
+
+
+    print(f'#{TC+1} {res}')
