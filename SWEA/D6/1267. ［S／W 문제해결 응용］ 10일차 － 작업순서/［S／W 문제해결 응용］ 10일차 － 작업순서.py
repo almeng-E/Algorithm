dc0c5fa6d7dc143283a2ac827d01a17a9641a269
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-02-16 16:44:20 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 127 ms, Memory: 76,624 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
@@ -1,36 +1,30 @@
-from collections import defaultdict, deque
-
-
+def dfs(v):
+    # graph, visited, in_degree --> result
+    visited[v] = True
+    result.append(v)
+    for next_node in graph[v]:
+        in_degree[next_node] -= 1
+        if in_degree[next_node] == 0 and not visited[next_node]:
+            dfs(next_node)
+ 
 T = 10
 for TC in range(T):
     V, E = map(int, input().split())
     edges = list(map(int, input().split()))
-
-    graph = defaultdict(list)
+ 
+    graph = [[] for _ in range(V+1)]
+    visited = [False for _ in range(V+1)]
     # 진입 차수
-    in_degree = defaultdict(int)
-
+    in_degree = [0 for _ in range(V+1)]
     # 그래프 저장
     for i in range(0, 2*E, 2):
         graph[edges[i]].append(edges[i+1])
         in_degree[edges[i+1]] += 1
-
+ 
     result = []
-
-    # queue 풀이
-    queue = deque()
-    for i in range(1, V+1):
-        if in_degree[i] == 0:
-            queue.append(i)
-    # queue 탐색
-    while queue:
-        node = queue.popleft()
-        result.append(node)
-
-        for next_node in graph[node]:
-            in_degree[next_node] -= 1
-            
-            if in_degree[next_node] == 0:
-                queue.append(next_node)
-
+    # dfs
+    for node in range(1, V+1):
+        if in_degree[node] == 0 and not visited[node]:
+            dfs(node)
+ 
     print(f'#{TC+1}', *result)
