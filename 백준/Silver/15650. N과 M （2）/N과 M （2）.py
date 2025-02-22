aa4e827ae8d2e977b646d39f9dbd8b66b20698af
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-02-23 02:26:21 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,18 +1,8 @@
-def make_combinations(c_index, c_subset):
-    if len(c_subset) == M:
-        print(*c_subset)
-        return
-
-    if c_index < N:
-        c_subset.append(nums[c_index])
-        make_combinations(c_index+1, c_subset)
-        c_subset.pop()
-        make_combinations(c_index+1, c_subset)
-
-
+from itertools import combinations
 
 N, M = map(int, input().split())
 
 nums = [i for i in range(1, N+1)]
 
-make_combinations(0, [])
+for comb in combinations(nums, M):
+    print(*comb)
