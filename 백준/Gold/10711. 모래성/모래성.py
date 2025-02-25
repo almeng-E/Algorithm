8598663d96a07086f829ed7943026e31f569c4a8
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-02-25 14:20:58 +0900

    [Gold II] Title: 모래성, Time: 1716 ms, Memory: 42520 KB -BaekjoonHub

--- 백준/Gold/10711. 모래성/모래성.py
+++ 백준/Gold/10711. 모래성/모래성.py
@@ -4,34 +4,35 @@
 # 를 바꿔 생각해서... 0이 되면 주위를 -1 한다...
 # 발상의 전환 미쳤어요
 from collections import deque
+def bfs():
+    # 큐 생성
+    queue = deque()
 
+    # 초기 보드 만들기
+    for i in range(H):
+        for j in range(W):
+            if board[i][j] == '.':
+                board[i][j] = 0
+                queue.append((i, j, 0))
+            else:
+                board[i][j] = int(board[i][j])
+
+    while queue:
+        x, y, wave = queue.popleft()
+
+        # 0 이면 주위의 높이를 내리자
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < H and 0 <= ny < W:
+                board[nx][ny] -= 1
+
+                if board[nx][ny] == 0:  # 다음 턴에 무너짐
+                    queue.append((nx, ny, wave + 1))
+    return wave
 steps = ((0, +1), (0, -1), (+1, 0), (-1, 0), (+1, +1), (-1, +1), (-1, -1), (+1, -1))
 
 H, W = map(int, input().split())
 
 board = [list(input()) for _ in range(H)]
 
-# 큐 생성
-queue = deque()
-
-# 초기 보드 만들기
-for i in range(H):
-    for j in range(W):
-        if board[i][j] == '.':
-            board[i][j] = 0
-            queue.append((i, j, 0))
-        else:
-            board[i][j] = int(board[i][j])
-
-while queue:
-    x, y, wave = queue.popleft()
-
-    # 0 이면 주위의 높이를 내리자
-    for dx, dy in steps:
-        nx, ny = x+dx, y+dy
-        if 0 <= nx < H and 0 <= ny < W:
-            board[nx][ny] -= 1
-
-            if board[nx][ny] == 0: # 다음 턴에 무너짐
-                queue.append((nx, ny, wave+1))
-print(wave)
+print(bfs())
