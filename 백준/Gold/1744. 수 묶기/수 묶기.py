2525488ef216c0243054a08382670e1bd7abddc3
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-02 13:51:47 +0900

    [Gold IV] Title: 수 묶기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -6,41 +6,32 @@ N = int(input())
 pos_nums = []
 neg_nums = []
 
+res = 0
 
 for _ in range(N):
     num = int(input())
-    if num > 0:
+    if num > 1:
         pos_nums.append(num)
-    else:
+    elif num <= 0:
         neg_nums.append(num)
+    else:
+        res += 1
 
 pos_nums.sort(reverse=True)
 neg_nums.sort()
 
-res = 0
-tmp = 0
-for i in range(0, len(pos_nums)):
-    # 1 은 규칙 제외
-    if pos_nums[i] == 1:
-        res += tmp
-        tmp = 0
-        res += 1
-        continue
 
-    if tmp == 0:
-        tmp = pos_nums[i]
+for i in range(0, len(pos_nums), 2):
+    if i+1 >= len(pos_nums):
+        res += pos_nums[i]
     else:
-        res += (tmp * pos_nums[i])
-        tmp = 0
-# 남아있는 수 합산
-res += tmp
-tmp = 0
-for i in range(0, len(neg_nums)):
-    if tmp == 0:
-        tmp = neg_nums[i]
+        res += (pos_nums[i] * pos_nums[i+1])
+
+
+for i in range(0, len(neg_nums), 2):
+    if i+1 >= len(neg_nums):
+        res += neg_nums[i]
     else:
-        res += (tmp * neg_nums[i])
-        tmp = 0
-res += tmp
+        res += (neg_nums[i] * neg_nums[i+1])
 
 print(res)
