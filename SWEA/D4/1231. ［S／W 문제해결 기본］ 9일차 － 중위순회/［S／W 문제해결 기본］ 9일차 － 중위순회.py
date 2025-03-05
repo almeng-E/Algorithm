1111a8aaabe57f9f0a4c576cd4592677b64def20
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-05 13:47:31 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 68 ms, Memory: 53,376 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,12 +1,19 @@
-def in_order(v):
-    if v not in tree:
+# Node 클래스 연습 저장용
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+
+
+def in_order(node):
+    if node == 0:
         return
 
-    if tree[v][1]:
-        in_order(tree[v][1][0])
-    print(tree[v][0], end="")
-    if len(tree[v][1]) == 2:
-        in_order(tree[v][1][1])
+    in_order(tree[node].left_node)
+    print(tree[node].data, end="")
+    in_order(tree[node].right_node)
 
 
 
@@ -19,8 +26,11 @@ for TC in range(10):
     # 정점 정보 입력
     for _ in range(N):
         a, b, *c = input().split()
-        tree[int(a)] = (b, list(map(int, c)))
-
+        if len(c) == 0:
+            c = [0, 0]
+        elif len(c) == 1:
+            c.append(0)
+        tree[int(a)] = Node(b, int(c[0]), int(c[1]))
 
     print(f'#{TC+1} ', end="")
     in_order(1)
