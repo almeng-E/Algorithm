a901c7bd922025d5eb19a244f6a85e8ba63e937c
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-12 22:39:09 +0900

    [D3] Title: N-Queen, Time: 127 ms, Memory: 61,440 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,53 +1,37 @@
-def dfs(x, y):
+def find(x, y):
     global res
-
-    if x == (N-1):
+    if x == N-1:
         res += 1
         return
 
-    # 마킹
-    for dx, dy in steps:
-        i = 1
-        while True:
-            nx, ny = x + dx*i, y + dy*i
-            if not(0 <= nx < N and 0 <= ny < N): break
+    # 방문 처리
+    v_verti[y] = True
+    v_diag_up[x+y] = True
+    v_diag_down[x-y] = True
 
-            visited[nx][ny] += 1
-            i += 1
-
-    # 가지치기: 다음 행에 안전한 칸이 전혀 없으면 백트래킹
-    safe_exists = any(visited[x+1][j] == 0 for j in range(N))
-    if safe_exists:
-        for j in range(N):
-            if visited[x+1][j] == 0:
-                dfs(x+1, j)
+    # 다음 호출 찾기
+    for j in range(N):
+        if not v_verti[j] and not v_diag_up[x+1+j] and not v_diag_down[x+1-j]:
+            find(x+1, j)
 
     # 백트래킹
-    for dx, dy in steps:
-        i = 1
-        while True:
-            nx, ny = x + dx * i, y + dy * i
-            if not (0 <= nx < N and 0 <= ny < N): break
-
-            visited[nx][ny] -= 1
-            i += 1
-
-
-
-steps = ((+1, 0), (+1, +1), (+1, -1))
+    v_verti[y] = False
+    v_diag_up[x + y] = False
+    v_diag_down[x - y] = False
+    
 T = int(input())
 for TC in range(T):
     N = int(input())
-
-    visited = [[0 for _ in range(N)] for _ in range(N)]
-
+    
+    # v_hori = [False] * N            # 인덱스 : i
+    v_verti = [False] * N           # 인덱스 : j
+    v_diag_up = [False] * (2*N)     # 인덱스 : i+j
+    v_diag_down = [False] * (2*N)   # 인덱스 : i-j
+    
+    
     res = 0
-
-    # 첫 줄로 체크하기
-    for i in range(N):
-        visited[0][i] = 1
-        dfs(0, i)
-        visited[0][i] = 0
-
-
+    for j in range(N):
+        find(0, j)  # i, j 인덱스
+    
+    
     print(f'#{TC+1} {res}')
