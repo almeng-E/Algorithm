c7b10b46c6bdba35fc442f913dbd7584d04d515b
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-13 14:55:14 +0900

    [Gold V] Title: 두 용액, Time: 120 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,28 @@
+N = int(input())
+liquid = list(map(int, input().split()))
+liquid.sort()
+
+st = 0
+ed = N-1
+
+min_val = max(abs(liquid[0]), abs(liquid[-1])) * 2 + 1
+result = []
+
+while st < ed:
+    S = liquid[st]
+    E = liquid[ed]
+    tmp = abs(S + E)
+
+    if tmp == 0:
+        result = [liquid[st], liquid[ed]]
+        break
+    if min_val > tmp:
+        min_val = tmp
+        result = [liquid[st], liquid[ed]]
+
+    if abs(S) >= abs(E):
+        st += 1
+    else:
+        ed -= 1
+
+print(*result)
