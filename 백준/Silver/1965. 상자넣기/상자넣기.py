f27edd55f6c628ed119fc87a6d4dc097a8b1c904
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-18 16:30:32 +0900

    [Silver II] Title: 상자넣기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,29 @@
+def bin_search(target):
+    left = 0
+    right = len(lis) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+        if target > lis[middle]:
+            left = middle+1
+        else:
+            right = middle-1
+
+    return left
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+lis = []
+lis.append(arr[0])
+
+for i in range(1, N):
+    if arr[i] > lis[-1]:
+        lis.append(arr[i])
+    else:
+        idx = bin_search(arr[i])
+        lis[idx] = arr[i]
+
+
+print(len(lis))
