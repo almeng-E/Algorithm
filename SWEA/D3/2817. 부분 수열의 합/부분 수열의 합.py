f5ce973a4d99d6842b9ec6f9c563f9129a2db3f0
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-18 11:53:54 +0900

    [D3] Title: 부분 수열의 합, Time: 1,882 ms, Memory: 78,976 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,36 +1,30 @@
-def find_subset(c_index, tmp_subset):
-    global A, result
-    # 가지치기
-    if sum(tmp_subset) > K:
-        return
-    elif sum(tmp_subset) == K:
-        result += 1
+def find(depth, tmp, l_idx):
+    global res
+    if depth >= N:
         return
 
-    # base-case
-    if c_index == N:
+    if tmp == K:
+        res += 1
         return
 
-
-    # 현재 요소를 포함하지 않고 넘기기
-    find_subset(c_index+1, tmp_subset)
-
-    # 현재 요소를 포함하고 넘기기
-    tmp_subset.append(A[c_index])
-    find_subset(c_index+1, tmp_subset)
-    # 원상복구 백트래킹
-    tmp_subset.pop()
-
-
+    for i in range(l_idx, N):
+        if not visited[i]:
+            visited[i] = True
+            find(depth+1, tmp + arr[i], i+1)
+            visited[i] = False
 
 
 T = int(input())
 for TC in range(T):
     N, K = map(int, input().split())
 
-    A = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
 
-    result = 0
-    find_subset(0, [])
+    res = 0
 
-    print(f'#{TC+1} {result}')
+    visited = [False] * N
+
+
+    find(0, 0, 0)
+
+    print(f'#{TC+1} {res}')
