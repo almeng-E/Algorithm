9f822dd049b2fd91f08566221cefebe1ec6485ee
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-19 11:49:45 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 105 ms, Memory: 80,896 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -1,25 +1,29 @@
+# DFS 풀이
 def dfs(v):
-
+    global cnt
     visited[v] = True
-    for next_node in graph[v]:
-        if not visited[next_node]:
-            dfs(next_node)
+    for n_node in graph[v]:
+        if not visited[n_node]:
+            dfs(n_node)
 
 
 T = int(input())
+
 for TC in range(T):
     N, M = map(int, input().split())
 
     graph = [[] for _ in range(N+1)]
-    visited = [False] * (N+1)
-    cnt = 0
+    visited = [False for _ in range(N+1)]
+
     for _ in range(M):
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
 
-    for i in range(1, N+1):
-        if not visited[i]:
+    cnt = 0
+    for node in range(1, N+1):
+        if not visited[node]:
+            dfs(node)
             cnt += 1
-            dfs(i)
+
     print(f'#{TC+1} {cnt}')
