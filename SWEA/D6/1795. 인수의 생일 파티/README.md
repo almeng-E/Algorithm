14c0c7efa9b9c2a948751736ce5663181182d7a0
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-21 00:48:27 +0900

    [D6] Title: 인수의 생일 파티, Time: 938 ms, Memory: 85,504 KB -BaekjoonHub

---
+++ SWEA/D6/1795. 인수의 생일 파티/README.md
@@ -0,0 +1,15 @@
+# [D6] 인수의 생일 파티 - 1795 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV4xuqCqBeUDFAUx) 
+
+### 성능 요약
+
+메모리: 85,504 KB, 시간: 938 ms, 코드길이: 986 Bytes
+
+### 제출 일자
+
+2025-03-21 00:48
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D6/1795. 인수의 생일 파티/인수의 생일 파티.py
@@ -0,0 +1,45 @@
+from heapq import heappop, heappush
+
+def djikstra(s):
+    global res
+    distance = [INF] * (N+1)
+
+    hq = []
+    heappush(hq, (0, s))
+    distance[s] = 0
+
+    while hq:
+        c_dist, c_node = heappop(hq)
+
+        if distance[c_node] < c_dist:
+            continue
+
+        for n_path in graph[c_node]:
+            n_node, weight = n_path
+
+            if distance[n_node] > c_dist + weight:
+                distance[n_node] = c_dist + weight
+                heappush(hq, (distance[n_node], n_node))
+
+    return distance
+
+INF = float('inf')
+T = int(input())
+for TC in range(T):
+    N, M, X = map(int, input().split())
+
+    graph = [[] for _ in range(N+1)]
+
+    for _ in range(M):
+        x, y, c = map(int, input().split())
+        graph[x].append((y, c))     # 다음 노드 , 가중치
+
+    res = 0
+    base_to_homes = djikstra(X)
+
+    for i in range(1, N+1):
+        if i != X:
+            tmp = djikstra(i)
+            res = max(res, tmp[X]+base_to_homes[i])
+
+    print(f'#{TC+1} {res}')
