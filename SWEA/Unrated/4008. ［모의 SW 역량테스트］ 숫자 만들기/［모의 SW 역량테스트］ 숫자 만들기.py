73e06754ecc70330ddc5c98631ee51f956213437
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-21 00:23:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 277 ms, Memory: 68,736 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.py
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.py
@@ -4,8 +4,8 @@ def recur(num_idx, tmp_res):
         # print(tmp_res)
         max_res = max(max_res, tmp_res)
         min_res = min(min_res, tmp_res)
-
-
+ 
+ 
     for i in range(4):
         if op_cnt[i] == 0:
             continue
@@ -20,21 +20,21 @@ def recur(num_idx, tmp_res):
             else:
                 recur(num_idx+1, int(tmp_res / cards[num_idx]))
             op_cnt[i] += 1
-
-
-
+ 
+ 
+ 
 T = int(input())
-
+ 
 for TC in range(T):
     N = int(input())
     # 0 : +, 1 : -, 2 : *, 3 : /
     op_cnt = list(map(int, input().split()))
-
+ 
     first_num, *cards = map(int, input().split())
-
+ 
     max_res = -float('inf')
     min_res = float('inf')
-
+ 
     recur(0, first_num)
-
+ 
     print(f'#{TC+1} {max_res - min_res}')
