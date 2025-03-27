79b662aa511696ebc2f96d11dabe4ca28f724458
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-28 01:01:17 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 136 ms, Memory: 45000 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -8,16 +8,13 @@ dp = [[],
       [0, 0, 1, 1],  # +2 로 끝나는 수의 개수
       [0, 0, 0, 1]]  # +3 로 끝나는 수의 개수
 
-T = int(input())
-Ns = [int(input()) for _ in range(T)]
+for _ in range(int(input())):
+    N = int(input())
+    if len(dp[1])-1 < N:
+        # dp 생성
+        for i in range(len(dp[1]), N+1):
+            dp[1].append((dp[2][i-1] + dp[3][i-1])% 1000000009)
+            dp[2].append((dp[1][i-2] + dp[3][i-2])% 1000000009)
+            dp[3].append((dp[1][i-3] + dp[2][i-3])% 1000000009)
 
-MAX = max(Ns)
-
-for i in range(4, MAX + 1):
-    dp[1].append((dp[2][i - 1] + dp[3][i - 1]) % 1000000009)
-    dp[2].append((dp[1][i - 2] + dp[3][i - 2]) % 1000000009)
-    dp[3].append((dp[1][i - 3] + dp[2][i - 3]) % 1000000009)
-
-
-for N in Ns:
-    print((dp[1][N] + dp[2][N] + dp[3][N]) % 1000000009)
+    print((dp[1][N] + dp[2][N] + dp[3][N])%1000000009)
