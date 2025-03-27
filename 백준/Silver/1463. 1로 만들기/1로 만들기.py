7c3e18c64c7b41a73b587b99eaa2f05821c83bce
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-03-27 17:44:15 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 187000 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,18 +1,18 @@
 import sys
 input = sys.stdin.readline
-
 N = int(input())
 dp = [0, 0]
-
-for i in range(2, N+1):
-    if i%3 == 0:
-        if i%2 != 0:
-            dp.append(min(dp[i//3], dp[i-1]) + 1)
+def find():
+    for i in range(2, N+1):
+        if i%3 == 0:
+            if i%2 != 0:
+                dp.append(min(dp[i//3], dp[i-1]) + 1)
+            else:
+                dp.append(min(dp[i//3], dp[i//2], dp[i-1]) + 1)
+        elif i%2 == 0:
+            dp.append(min(dp[i//2], dp[i-1]) + 1)
         else:
-            dp.append(min(dp[i//3], dp[i//2], dp[i-1]) + 1)
-    elif i%2 == 0:
-        dp.append(min(dp[i//2], dp[i-1]) + 1)
-    else:
-        dp.append(dp[i-1] + 1)
+            dp.append(dp[i-1] + 1)
 
-print(dp[N])
+    print(dp[N])
+find()
