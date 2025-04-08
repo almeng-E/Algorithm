fa44f48587dc671b19c52cda249945db26d865e0
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-04-08 15:02:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 2548 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -4,14 +4,14 @@ N, K = map(int, input().split())
 
 items = [list(map(int, input().split())) for _ in range(N)]
 
-DP = [0] * (K+1)
+bef_DP = [0] * (K+1)
 
-items.sort(key=lambda x: x[0])
 
 for item, value in items:
-    for kg in range(K, item-1, -1):
+    aft_DP = bef_DP[:]
+    for kg in range(item, K+1):
         # 넣는게 이득인지 판단
-        if DP[kg] < DP[kg - item] + value:
-            DP[kg] = DP[kg - item] + value
-
-print(DP[K])
+        if aft_DP[kg] < bef_DP[kg - item] + value:
+            aft_DP[kg] = bef_DP[kg - item] + value
+    bef_DP = aft_DP
+print(bef_DP[K])
