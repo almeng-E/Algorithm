97d38db8e87b32ad44a46cc897f76c7490082688
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-04-12 16:38:52 +0900

    [D3] Title: XY 문자열 1, Time: 62 ms, Memory: 59,264 KB -BaekjoonHub

--- SWEA/D3/20955. XY 문자열 1/XY 문자열 1.py
+++ SWEA/D3/20955. XY 문자열 1/XY 문자열 1.py
@@ -1,26 +1,13 @@
 T = int(input())
 for TC in range(T):
-    S = input().rstrip()
-    E = input().rstrip()
-
-    is_reversed = 0
-
-    while len(S) < len(E):
-        if not is_reversed:
-            if E[-1] == 'X':
-                E = E[:-1]
-            else:
-                E = E[:-1]
-                is_reversed ^= 1
+    S = list(input().rstrip())
+    E = list(input().rstrip())
+    while len(E) > len(S):
+        if E[-1] == 'X':
+            E.pop()
         else:
-            if E[0] == 'X':
-                E = E[1:]
-            else:
-                E = E[1:]
-                is_reversed ^= 1
-    if is_reversed:
-        E = E[::-1]
-
+            E.pop()
+            E.reverse()
     if E == S:
         print(f'#{TC+1} Yes')
     else:
