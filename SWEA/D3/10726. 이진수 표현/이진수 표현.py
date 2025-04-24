7881b72416d85a8f82801556294dceca1bc5019b
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-04-24 15:15:24 +0900

    [D3] Title: 이진수 표현, Time: 574 ms, Memory: 64,256 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.py
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -1,13 +1,12 @@
+
 T = int(input())
-for TC in range(T):
+for TC in range(1, T+1):
     N, M = map(int, input().split())
-
-
+    ON = True
     for i in range(N):
         if M & 1 << i:
             continue
         else:
-            print(f'#{TC+1} OFF')
+            ON = False
             break
-    else:
-        print(f'#{TC+1} ON')
+    print(f'#{TC}', 'ON' if ON else 'OFF')
