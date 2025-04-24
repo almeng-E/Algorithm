bfe162623694d5b15058bb6e7b1fd77b2d3bfa78
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-04-24 14:13:21 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 69 ms, Memory: 59,008 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -0,0 +1,16 @@
+T = int(input())
+for TC in range(1, T+1):
+    N = int(input())
+
+    found = 0
+    cnt = 1
+    while found != (1 << 10)-1:
+        num = N*cnt
+
+        while num:
+            found = found | (1 << (num % 10))
+            num //= 10
+        cnt += 1
+
+
+    print(f'#{TC} {(cnt-1)*N}')
