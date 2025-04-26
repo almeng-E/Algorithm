4610d132808baf8d58aaa0898b46fca727262aef
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-04-26 23:09:35 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 164 ms, Memory: 69,376 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.py
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.py
@@ -18,15 +18,40 @@ for TC in range(T):
         graph[arr[i]].append(arr[i+1])
 
     depth = [0] * (V+1)
-    par = [1] * (V+1)
+    par = [0] * (V+1)
     size = [1] * (V+1)
+    dfs(1, 0)
 
-    dfs(1, 1)
-    while depth[A] > depth[B]:
+    # sparce table을 채워보자
+    LOG = V.bit_length()
+
+    up = [[0 for _ in range(LOG)] for _ in range(V+1)]
+    # 직접 부모
+    for i in range(1, V+1):
+        up[i][0] = par[i]
+
+    # 나머지 sparce table 채우기
+    for j in range(1, LOG):
+        for i in range(1, V+1):
+            up[i][j] = up[ up[i][j-1] ][ j-1 ]
+            # mid = up[i][j-1]
+            # up[i][j] = up[mid][j-1]
+
+    # LCA - binary lifting
+    # A가 더 깊음
+    if depth[A] < depth[B]:
+        A, B = B, A
+    diff = depth[A] - depth[B]
+    # 2진수 분해 점프
+    for k in range(LOG-1, -1, -1):
+        if diff & (1 << k):
+            A = up[A][k]
+
+    if A != B:
+        for i in range(LOG-1, -1, -1):
+            if up[A][i] != up[B][i]:
+                A = up[A][i]
+                B = up[B][i]
         A = par[A]
-    while depth[A] < depth[B]:
-        B = par[B]
-    while A != B:
-        A = par[A]
-        B = par[B]
+
     print(f'#{TC+1} {A} {size[A]}')
