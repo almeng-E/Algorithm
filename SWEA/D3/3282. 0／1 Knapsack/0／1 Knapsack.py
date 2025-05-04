8857d20105f20eac13f77ac09e661539c37dc987
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-05-04 15:59:39 +0900

    [D3] Title: 0/1 Knapsack, Time: 121 ms, Memory: 66,432 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
@@ -1,19 +1,3 @@
-def choice(idx, curr_weight):
-    if curr_weight > K:
-        return -float('inf')
-
-    if idx == N:
-        return 0
-
-    if DP[idx][curr_weight] != -1:
-        return DP[idx][curr_weight]
-
-    a = item[idx][1] + choice(idx+1, curr_weight + item[idx][0])
-    b = choice(idx+1, curr_weight)
-
-    DP[idx][curr_weight] = max(a, b)
-    return DP[idx][curr_weight]
-
 T = int(input())
 for TC in range(1, T+1):
     N, K = map(int, input().split())
@@ -21,8 +5,13 @@ for TC in range(1, T+1):
     for _ in range(N):
         item.append(tuple(map(int, input().split())))
 
-    DP = [[-1 for _ in range(K+1)] for _ in range(N)]
+    DP = [[0 for _ in range(K+1)] for _ in range(N+1)]
 
-    max_v = choice(0, 0)
+    for i in range(N-1, -1, -1):
+        w, v = item[i]
+        for j in range(K, -1, -1):
+            take = v + (DP[i+1][j-w] if j>=w else -float('inf'))
+            skip = DP[i+1][j]
+            DP[i][j] = max(take, skip)
 
-    print(f'#{TC} {max_v}')
+    print(f'#{TC} {DP[0][K]}')
