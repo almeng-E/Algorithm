25075f552140791c2b291dd8e333150a50d9ec4e
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-05-28 13:25:32 +0900

    [Gold IV] Title: 전화번호 목록, Time: 768 ms, Memory: 47768 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+class Node:
+    def __init__(self):
+        self.is_terminal = False
+        self.child = {}
+
+def sol():
+    N = int(input())
+
+    arr = list(input().rstrip() for _ in range(N))
+    arr.sort(key=len)
+    # print(arr)
+
+    for num in arr:
+        cur = ROOT
+        for s in num:
+            if s not in cur.child:
+                cur.child[s] = Node()
+
+            if cur.is_terminal:
+                return False
+
+            cur = cur.child[s]
+        cur.is_terminal = True
+
+    return True
+
+
+T = int(input())
+
+
+for _ in range(T):
+    ROOT = Node()
+
+    res = sol()
+    print('YES' if res else 'NO')
