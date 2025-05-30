00ecf1eeedc45803922ed515aba671ff0ecfa85d
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-05-30 14:55:02 +0900

    [Platinum III] Title: 전설, Time: 1184 ms, Memory: 112696 KB -BaekjoonHub

--- 백준/Platinum/19585. 전설/전설.py
+++ 백준/Platinum/19585. 전설/전설.py
@@ -1,48 +1,67 @@
 import sys
 input = sys.stdin.readline
-class Node:
-    def __init__(self):
-        self.is_terminal = False
-        self.child = {}
 
-C, N = map(int, input().split())
+# 라빈-카프 스러운 Rolling-hash
+N, M = map(int, input().split())
 
-c_root = Node()
+base = 97
+MOD = 10**9 + 9
 
-# color-trie
-for _ in range(C):
-    cur = c_root
+inv_b = pow(base, MOD-2, MOD)
+
+
+# 색상 입력 받기
+colors = [set() for _ in range(2001)]
+for _ in range(N):
     word = input().rstrip()
-    for s in word:
-        if s not in cur.child:
-            cur.child[s] = Node()
-        cur = cur.child[s]
+    val = 0
+    L = len(word)
+    for i in range(L):
+        val *= base
+        val += ord(word[i])
+        val %= MOD
+    colors[L].add(val)
 
-    cur.is_terminal = True
+# 이름 입력 받기
+names = [set() for _ in range(2001)]
+for _ in range(M):
+    word = input().rstrip()
+    val = 0
+    L = len(word)
+    # 거꾸로
+    for i in range(L-1, -1, -1):
+        val *= base
+        val += ord(word[i])
+        val %= MOD
+    names[L].add(val)
 
-names = set(input().rstrip() for _ in range(N))
 
-# search
+# 검색하기
 Q = int(input())
 for _ in range(Q):
-    res = False
     word = input().rstrip()
-    cur = c_root
-    found = False
-    for i in range(len(word)):
-        # color-trie 탐색
-        if word[i] in cur.child:
-            # 다음 노드가 있음
-            cur = cur.child[word[i]]
-        else:
-            # 없음 => 색 오류
+    L = len(word)
+
+    # 초기 해시 (\ /)
+    right = 0
+    for i in range(L-1, -1, -1):
+        right *= base
+        right += ord(word[i])
+        right %= MOD
+
+    left = 0
+    ok = False
+
+    # rolling - hash 접두 접미 검색
+    for i in range(L-1):
+        left *= base
+        left += ord(word[i])
+        left %= MOD
+
+        right = ((right - ord(word[i]) + MOD) * inv_b) % MOD
+
+        if left in colors[i+1] and right in names[L-i-1]:
+            ok = True
             break
 
-        if cur.is_terminal:
-            # 색의 후보 -> 이름 탐색
-            if word[i+1:] in names:
-                res = True
-                break
-
-
-    print('Yes' if res else 'No')
+    print('Yes' if ok else 'No')
