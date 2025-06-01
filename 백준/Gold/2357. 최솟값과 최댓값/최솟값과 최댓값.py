9d8ce86e870dccbbae385de5afe75802915fc307
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-06-01 21:14:59 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 1400 ms, Memory: 66964 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
@@ -1,15 +1,14 @@
 import sys
 input = sys.stdin.readline
-
 N, M = map(int, input().split())
-
+INF = 10**10
 LEN = 1
 while LEN < N:
     LEN *= 2
 SIZE = LEN * 2
 
 # 트리 생성
-tree = [(float('inf'), -float('inf')) for _ in range(SIZE)]  # (최솟값, 최댓값)
+tree = [(INF, -INF) for _ in range(SIZE)]  # (최솟값, 최댓값)
 
 # 트리 채우기 - 1 : leaf 채우기
 for i in range(N):
@@ -22,10 +21,12 @@ for i in range(LEN-1, 0, -1):
     mx = max(tree[i*2][1], tree[i*2 + 1][1])
     tree[i] = (mn, mx)
 
+
 # 쿼리 처리하기 (투포인터 탐색)
+out = []
 for _ in range(M):
-    tmp_min = float('inf')
-    tmp_max = -float('inf')
+    tmp_min = INF
+    tmp_max = -INF
 
     left, right = map(int, input().split())
     left = left - 1 + LEN   # 0-index
@@ -43,4 +44,8 @@ for _ in range(M):
 
         left //= 2
         right //= 2
-    print(tmp_min, tmp_max)
+
+    # 결과를 문자열 형태로 모아 두기
+    out.append(f"{tmp_min} {tmp_max}")
+    
+sys.stdout.write("\n".join(out))
