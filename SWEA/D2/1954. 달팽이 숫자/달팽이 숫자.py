44f0e81e117ee5dd15b2f9b2677bad5d81419109
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-06-03 16:26:49 +0900

    [D2] Title: 달팽이 숫자, Time: 64 ms, Memory: 54,016 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,32 +1,22 @@
-def snail(N):
-    board = [[0 for _ in range(N)] for _ in range(N)]
-
-    #       우       하       좌       상
-    steps = ((0, +1), (+1, 0), (0, -1), (-1, 0))
-
-    cnt = 1
-    curr_x, curr_y = 0, 0
-    while True:
-       # 이동
-        for dx, dy in steps:
-            while True:
-                board[curr_x][curr_y] = cnt
-                if cnt >= N**2:
-                    return board
-                if 0 <= curr_x + dx < N and 0 <= curr_y + dy < N and board[curr_x + dx][curr_y + dy] == 0:
-                    curr_x, curr_y = curr_x + dx, curr_y + dy
-                    cnt += 1
-                else:
-                    break
-
-
-
 T = int(input())
+steps = ((0, 1), (1, 0), (0, -1), (-1, 0))
 for TC in range(T):
     N = int(input())
-    board = snail(N)
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    di = 0
+    v = 1
+    x, y = 0, 0
+    while v <= N**2:
+        board[x][y] = v
 
+        nx, ny = x + steps[di][0], y + steps[di][1]
+        if nx < 0 or nx >= N or ny < 0 or ny >= N or board[nx][ny]:
+            di += 1
+            di %= 4
+
+        x, y = x + steps[di][0], y + steps[di][1]
+        v += 1
 
     print(f'#{TC+1}')
-    for i in range(len(board)):
-        print(*board[i])
+    for row in board:
+        print(*row)
