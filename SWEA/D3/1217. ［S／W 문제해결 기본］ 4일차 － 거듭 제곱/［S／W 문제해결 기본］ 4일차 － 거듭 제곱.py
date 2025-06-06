0ace0ae9c983eeadef3d56eddf26d3ece4fa0eee
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-06-06 18:45:11 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 64 ms, Memory: 50,048 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,4 +1,14 @@
+def power(base, exponent):
+    if exponent == 0:
+        return 1
+
+    if exponent & 1:
+        return base * (power(base, exponent//2) ** 2)
+    else:
+        return power(base, exponent//2) ** 2
+
+
 for _ in range(10):
     TC = int(input())
     N, M = map(int, input().split())
-    print(f'#{TC} {pow(N, M)}')
+    print(f'#{TC} {power(N, M)}')
