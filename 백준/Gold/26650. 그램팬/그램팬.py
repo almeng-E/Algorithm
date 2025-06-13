3610ea1d8e3644048be94f93cb1bcd24395a671a
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-06-13 23:11:45 +0900

    [Gold V] Title: 그램팬, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/26650. 그램팬/그램팬.py
+++ 백준/Gold/26650. 그램팬/그램팬.py
@@ -1,37 +1,40 @@
 import sys
 input = sys.stdin.readline
 
-S = input().rstrip()
-n = len(S)
-res = 0
-st = 0
 
-while st < n:
-    if S[st] != 'A':
-        st += 1
-        continue
+def main():
+    S = input().rstrip()
+    n = len(S)
+    res = 0
+    st = 0
+    while st < n:
+        if S[st] != 'A':
+            st += 1
+            continue
 
-    ed = st
-    a_cnt = 0
-    z_cnt = 0
-    prev = chr(64)
+        ed = st
+        a_cnt = 0
+        z_cnt = 0
+        prev = chr(64)
 
-    while ed < n:
-        curr = S[ed]
-        if ord(curr) - ord(prev) not in (0, 1):
-            break
+        while ed < n:
+            curr = S[ed]
+            if ord(curr) - ord(prev) not in (0, 1):
+                break
 
-        if S[ed] == 'A':
-            a_cnt += 1
-        elif S[ed] == 'Z':
-            z_cnt += 1
+            if S[ed] == 'A':
+                a_cnt += 1
+            elif S[ed] == 'Z':
+                z_cnt += 1
 
-        prev = curr
-        ed += 1
+            prev = curr
+            ed += 1
 
-    if a_cnt > 0 and z_cnt > 0:
-        res += a_cnt * z_cnt
+        if a_cnt > 0 and z_cnt > 0:
+            res += a_cnt * z_cnt
+        
+        # 점프
+        st = ed
 
-    st = ed
-
-print(res)
+    print(res)
+main()
