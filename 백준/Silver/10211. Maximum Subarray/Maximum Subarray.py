dbb63dbd98bbfb43ffd60d62838566175a82a520
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-06-15 18:27:26 +0900

    [Silver IV] Title: Maximum Subarray, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
@@ -1,18 +1,14 @@
 import sys
 input = sys.stdin.readline
+
 T = int(input())
 for _ in range(T):
     N = int(input())
     arr = list(map(int, input().split()))
 
-    sub = [0]
-    tmp = 0
-    for i in arr:
-        tmp += i
-        sub.append(tmp)
+    max_sum = current_sum = arr[0]
+    for i in range(1, N):
+        current_sum = max(arr[i], current_sum + arr[i])
+        max_sum = max(max_sum, current_sum)
 
-    res = -float('inf')
-    for i in range(N+1):
-        for j in range(i):
-            res = max(sub[i]-sub[j], res)
-    print(res)
+    print(max_sum)
