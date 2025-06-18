9ed5d9eff1354a79d42eb050c875bdd6fb002d54
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-06-18 15:32:06 +0900

    [Gold I] Title: 수들의 합 7, Time: 6516 ms, Memory: 86684 KB -BaekjoonHub

--- 백준/Gold/2268. 수들의 합 7/수들의 합 7.py
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.py
@@ -1,45 +1,47 @@
 import sys
 input = sys.stdin.readline
+def main():
+    N, M = map(int, input().split())
 
-N, M = map(int, input().split())
+    LEN = 1
+    while LEN < N:
+        LEN <<= 1
+    SIZE = LEN << 1
 
-LEN = 1
-while LEN < N:
-    LEN <<= 1
-SIZE = LEN << 1
-
-tree = [0] * SIZE
+    tree = [0] * SIZE
 
 
-for _ in range(M):
-    cmd, a, b = map(int, input().split())
+    for _ in range(M):
+        cmd, a, b = map(int, input().split())
 
-    if cmd == 0:
-        # get-range
-        if a > b:
-            a, b = b, a
+        if cmd == 0:
+            # get-range
+            if a > b:
+                a, b = b, a
 
-        left = a + LEN - 1
-        right = b + LEN - 1
-        res = 0
+            left = a + LEN - 1
+            right = b + LEN - 1
+            res = 0
 
-        while left <= right:
-            if left & 1:
-                res += tree[left]
-                left += 1
-            if not right & 1:
-                res += tree[right]
-                right -= 1
-            left >>= 1
-            right >>= 1
+            while left <= right:
+                if left & 1:
+                    res += tree[left]
+                    left += 1
+                if not right & 1:
+                    res += tree[right]
+                    right -= 1
+                left >>= 1
+                right >>= 1
 
-        print(res)
+            print(res)
 
-    elif cmd == 1:
-        # update-point
-        idx = a + LEN - 1
-        tree[idx] = b
-        idx >>= 1
-        while idx:
-            tree[idx] = tree[(idx << 1)] + tree[(idx << 1) + 1]
+        elif cmd == 1:
+            # update-point
+            idx = a + LEN - 1
+            tree[idx] = b
             idx >>= 1
+            while idx:
+                tree[idx] = tree[(idx << 1)] + tree[(idx << 1) + 1]
+                idx >>= 1
+
+main()
