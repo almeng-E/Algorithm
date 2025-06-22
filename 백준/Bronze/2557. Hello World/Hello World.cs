ad148478b5af25d473e11801d0e3c9e10c27cd49
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-06-23 00:25:28 +0900

    [Bronze V] Title: Hello World, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace HELLOWORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
