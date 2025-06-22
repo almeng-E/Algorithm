bf4a0362e47b25d23cb93eefe1fc5fd9823396b2
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-06-23 01:27:55 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine(int.Parse(s[0]) + int.Parse(s[1]));
+
+
+
+        }
+    }
+}
