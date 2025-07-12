18af547200366883398cb197db6577f46ad6e245
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-07-12 23:23:21 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long A, B, C;
+    cin >> A >> B >> C;
+    cout << A + B + C;
+
+
+
+    return 0;
+}
+
