aa082307a676fe83573c20e9f726120aec275628
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-08-01 09:14:01 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << "I'm Sexy";
+    return 0;
+}
