745dd77abad6ffd2bd8364aaf4f4d27657a2487e
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-08-22 00:52:50 +0900

    [Platinum IV] Title: 휴대폰 자판, Time: 2316 ms, Memory: 116376 KB -BaekjoonHub

---
+++ 백준/Platinum/5670. 휴대폰 자판/휴대폰 자판.py
@@ -0,0 +1,56 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+
+class Node:
+    def __init__(self):
+        self.children = dict()
+        self.isTerminal = False
+
+
+def dfs(node, depth):
+    global res
+
+    while len(node.children) == 1 and not node.isTerminal:
+        k = list(node.children.keys())[0]
+        node = node.children[k]
+
+    if node.isTerminal:
+        res += depth
+
+    for ch in node.children.keys():
+        nxt = node.children[ch]
+        dfs(nxt, depth+1)
+
+while True:
+    try:
+        ROOT = Node()
+        # 트라이 구성
+        N = int(input())
+        for _ in range(N):
+            S = input().rstrip()
+            CUR = ROOT
+            for ch in S:
+                if ch not in CUR.children:
+                    nxt = Node()
+                    CUR.children[ch] = nxt
+
+                CUR = CUR.children[ch]
+            CUR.isTerminal = True
+
+
+
+
+
+
+        # DFS
+        res = 0
+        for inp in ROOT.children.keys():
+            dfs(ROOT.children[inp], 1)
+
+        print(f"{round(res/N, 2):.2f}")
+
+
+    except:
+        break
