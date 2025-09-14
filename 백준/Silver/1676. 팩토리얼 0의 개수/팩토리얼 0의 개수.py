d3da7402272f78a59cb81a7b4c19616a1f9a9eae
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-09-14 14:25:34 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -0,0 +1,17 @@
+N = int(input())
+two = 0
+five = 0
+for i in range(1, N+1):
+    while i%10 == 0:
+        two += 1
+        five += 1
+        i //= 10
+    while i%2 == 0:
+        two += 1
+        i //= 2
+    while i%5 == 0:
+        five += 1
+        i //= 5
+
+
+print(min(two, five))
