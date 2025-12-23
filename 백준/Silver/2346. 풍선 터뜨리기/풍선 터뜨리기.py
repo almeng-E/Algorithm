7219cb83bac09ba3a7a29ee919067ec189982830
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-12-24 01:26:49 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 76 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,24 +1,24 @@
 import sys
 input = sys.stdin.readline
-
-
 from collections import deque
 
-N = int(input())
-arr = list(map(int, input().split()))
-q = deque()
-for i in range(N):
-    q.append((i+1, arr[i]))
-
-ret = []
-for _ in range(N-1):
-    cur, move = q.popleft()
-    ret.append(cur)
-    if move > 0:
-        for _ in range(move-1):
-            q.append(q.popleft())
-    else:
-        for _ in range(-move):
-            q.appendleft(q.pop())
-ret.append(q[0][0])
-print(*ret)
+def solve():
+    N = int(input())
+    arr = list(map(int, input().split()))
+    q = deque()
+    for i in range(N):
+        q.append((i+1, arr[i]))
+    
+    ret = []
+    for _ in range(N-1):
+        cur, move = q.popleft()
+        ret.append(cur)
+        if move > 0:
+            for _ in range(move-1):
+                q.append(q.popleft())
+        else:
+            for _ in range(-move):
+                q.appendleft(q.pop())
+    ret.append(q[0][0])
+    print(*ret)
+solve()
