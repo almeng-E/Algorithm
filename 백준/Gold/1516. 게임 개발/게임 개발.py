82a5dacb447c794f14da04b12eb096dec041fd19
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-12-26 22:24:22 +0900

    [Gold III] Title: 게임 개발, Time: 84 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.py
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -1,33 +1,34 @@
 import sys
 input = sys.stdin.readline
 
+from collections import deque
+
 N = int(input())
 t = [0] * (N+1)
 g = [[] for _ in range(N+1)]
 dp = [0] * (N+1)
-s_nodes = []
+in_degree = [0] * (N+1)
 
 for i in range(1, N+1):
     time, *prv, end = map(int, input().split())
     t[i] = time
+    in_degree[i] = len(prv)
     for p in prv:
         g[p].append(i)
-    if not prv:
-        s_nodes.append(i)
 
+q = deque()
+for i in range(1, N+1):
+    if in_degree[i] == 0:
+        q.append(i)
+        dp[i] = t[i]
 
-def dfs(cur):
+while q:
+    cur = q.popleft()
     for nxt in g[cur]:
+        in_degree[nxt] -= 1
         if dp[nxt] < dp[cur] + t[nxt]:
             dp[nxt] = dp[cur] + t[nxt]
-            dfs(nxt)
+        if in_degree[nxt] == 0:
+            q.append(nxt)
 
-
-for s in s_nodes:
-    dp[s] = t[s]
-
-for s in s_nodes:
-    dfs(s)
-
-for ret in dp[1:]:
-    print(ret)
+print(*dp[1:], sep='\n')
