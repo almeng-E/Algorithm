6ee23e339c4e5a69a531e50bc534dca8af681659
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2025-12-27 17:41:35 +0900

    [Platinum V] Title: 상남자, Time: 1292 ms, Memory: 48980 KB -BaekjoonHub

--- 백준/Platinum/17267. 상남자/상남자.py
+++ 백준/Platinum/17267. 상남자/상남자.py
@@ -1,57 +1,58 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-from collections import deque
 
 N, M = map(int, input().split())
 L, R = map(int, input().split())
 board = [list(map(int, input().rstrip())) for _ in range(N)]
 sx, sy = -1, -1
 
+def solve():
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 2:
+                sx, sy = i, j
+                break
 
-for i in range(N):
-    for j in range(M):
-        if board[i][j] == 2:
-            sx, sy = i, j
-            break
+    v = [[0] * M for _ in range(N)]
+    v[sx][sy] = 1
+    res = 1
+    q = deque()
+    q.append((sx, sy, L, R))
+    while q:
+        x, y, l, r = q.popleft()
 
-v = [[0] * M for _ in range(N)]
-v[sx][sy] = 1
-res = 1
-q = deque()
-q.append((sx, sy, L, R))
-while q:
-    x, y, l, r = q.popleft()
-
-    # 좌
-    if l > 0 and y-1 >= 0 and board[x][y-1] == 0 and v[x][y-1] == 0:
-        q.append((x, y-1, l-1, r))
-        v[x][y-1] = 1
-        res += 1
-    # 우
-    if r > 0 and y+1 < M and board[x][y+1] == 0 and v[x][y+1] == 0:
-        q.append((x, y+1, l, r-1))
-        v[x][y+1] = 1
-        res += 1
-    # 상
-    nx = x
-    while nx >= 0:
-        if board[nx][y] == 0 and v[nx][y] == 0:
-            q.append((nx, y, l, r))
-            v[nx][y] = 1
+        # 좌
+        if l > 0 and y-1 >= 0 and board[x][y-1] == 0 and v[x][y-1] == 0:
+            q.append((x, y-1, l-1, r))
+            v[x][y-1] = 1
             res += 1
-        elif board[nx][y] == 1:
-            break
-        nx -= 1
-
-    # 하
-    nx = x
-    while nx < N:
-        if board[nx][y] == 0 and v[nx][y] == 0:
-            q.append((nx, y, l, r))
-            v[nx][y] = 1
+        # 우
+        if r > 0 and y+1 < M and board[x][y+1] == 0 and v[x][y+1] == 0:
+            q.append((x, y+1, l, r-1))
+            v[x][y+1] = 1
             res += 1
-        elif board[nx][y] == 1:
-            break
-        nx += 1
-print(res)
+        # 상
+        nx = x
+        while nx >= 0:
+            if board[nx][y] == 0 and v[nx][y] == 0:
+                q.append((nx, y, l, r))
+                v[nx][y] = 1
+                res += 1
+            elif board[nx][y] == 1:
+                break
+            nx -= 1
+
+        # 하
+        nx = x
+        while nx < N:
+            if board[nx][y] == 0 and v[nx][y] == 0:
+                q.append((nx, y, l, r))
+                v[nx][y] = 1
+                res += 1
+            elif board[nx][y] == 1:
+                break
+            nx += 1
+    print(res)
+
+solve()
