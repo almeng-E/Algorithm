0e4537a362fcc30d393c7224a85c89bea7f626e2
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-01-03 21:12:31 +0900

    [Gold IV] Title: 나는 정말 휘파람을 못 불어, Time: 1688 ms, Memory: 61644 KB -BaekjoonHub

---
+++ 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+MOD = 1_000_000_007
+N = int(input())
+S = '_' + input().rstrip()
+
+# W, WH, WHE
+WHEEs = 0
+dp = [[0 for _ in range(3)] for _ in range(N+1)]
+for i in range(1, N+1):
+    tg = 5
+    if S[i] == 'W':
+        tg = 0
+        dp[i][0] = dp[i-1][0] + 1
+    elif S[i] == 'H':
+        tg = 1
+        dp[i][1] = dp[i-1][1] + dp[i-1][0]
+
+    elif S[i] == 'E':
+        tg = 2
+        WHEEs *= 2
+        WHEEs += dp[i-1][2]
+        dp[i][2] = dp[i-1][2] + dp[i-1][1]
+
+    for j in range(3):
+        if tg == j:
+            continue
+        dp[i][j] = dp[i-1][j]
+
+print(WHEEs % MOD)
