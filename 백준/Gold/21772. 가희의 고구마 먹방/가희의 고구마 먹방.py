c5a6c20b2cc0ee94e89bfc962d260b3cbbac03dc
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-01-05 00:25:06 +0900

    [Gold V] Title: 가희의 고구마 먹방, Time: 376 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/21772. 가희의 고구마 먹방/가희의 고구마 먹방.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
+def find(r, c):
+    for i in range(r):
+        for j in range(c):
+            if board[i][j] == 'G':
+                return i, j
+
+
+def backtrack(x, y, sp, cur):
+    global T, ret, steps, R, C, board
+    if cur == T:
+        ret = max(ret, sp)
+        return
+    for dx, dy in steps:
+        nx, ny = x+dx, y+dy
+        if nx < 0 or nx >= R or ny < 0 or ny >= C:
+            continue
+        if board[nx][ny] == '#':
+            continue
+        elif board[nx][ny] == 'S':
+            board[nx][ny] = '.'
+            backtrack(nx, ny, sp+1, cur+1)
+            board[nx][ny] = 'S'
+        else:
+            backtrack(nx, ny, sp, cur+1)
+
+
+steps = ((0, 1), (0, -1), (1, 0), (-1, 0))
+R, C, T = map(int, input().split())
+board = [list(input()) for _ in range(R)]
+
+sx, sy = find(R, C)
+
+ret = 0
+backtrack(sx, sy, 0, 0)
+print(ret)
