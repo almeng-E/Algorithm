f79f78e4fd79bb15cb5898faa8676588ab664a75
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-01-09 12:30:57 +0900

    [Gold I] Title: 뭉쳐야 산다, Time: 1872 ms, Memory: 190088 KB -BaekjoonHub

---
+++ 백준/Gold/28277. 뭉쳐야 산다/뭉쳐야 산다.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+N, Q = map(int, input().split())
+S = [set() for _ in range(N+1)]
+for i in range(1, N+1):
+    _, *arr = map(int, input().split())
+    S[i] = set(arr)
+
+for _ in range(Q):
+    cmd = list(map(int, input().split()))
+    if cmd[0] == 1:
+        a, b = cmd[1:]
+        if len(S[a]) < len(S[b]):
+            S[a], S[b] = S[b], S[a]
+        S[a] |= S[b]
+        S[b].clear()
+    else:
+        a = cmd[1]
+        print(len(S[a]))
