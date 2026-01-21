9617bd890847e85418d26ff5d7b9be344dcdc099
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-01-21 18:27:55 +0900

    [Platinum II] Title: Frequent values, Time: 1764 ms, Memory: 186980 KB -BaekjoonHub

---
+++ 백준/Platinum/6515. Frequent values/Frequent values.py
@@ -0,0 +1,66 @@
+import sys
+input = sys.stdin.readline
+
+def add(idx):
+    global max_cnt
+    C = color[idx]
+    cnt_cnt[C_cnt[C]] -= 1
+    C_cnt[C] += 1
+    cnt_cnt[C_cnt[C]] += 1
+    if max_cnt < C_cnt[C]:
+        max_cnt = C_cnt[C]
+
+
+def remove(idx):
+    global max_cnt
+
+    C = color[idx]
+    cc = C_cnt[C]
+    C_cnt[C] -= 1
+    cnt_cnt[cc] -= 1
+    if max_cnt == cc and cnt_cnt[cc] == 0:
+        max_cnt -= 1
+    cnt_cnt[C_cnt[C]] += 1
+
+
+while True:
+    try:
+        N, M = map(int, input().split())
+    except:
+        break
+    color = [0] + list(map(int, input().split()))
+    Q = []
+    for i in range(M):
+        X, Y = map(int, input().split())
+        Q.append((X, Y, i))
+
+    sqrtN = int(N**0.5)
+    Q.sort(key=lambda x: (x[0]//sqrtN, x[1]))
+
+
+    ans = [0] * M
+    C_cnt = [0] * 200_002
+    cnt_cnt = [0] * 100_001
+    max_cnt = 0
+
+    l, r = 1, 0
+    for ql, qr, idx in Q:
+        while l < ql:
+            remove(l)
+            l += 1
+
+        while ql < l:
+            l -= 1
+            add(l)
+
+        while r < qr:
+            r += 1
+            add(r)
+
+        while qr < r:
+            remove(r)
+            r -= 1
+
+        ans[idx] = max_cnt
+
+    print('\n'.join(map(str, ans)))
