83a20e09a0870b0c67462b72048fcb618030aabc
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-02-20 00:29:44 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 2500 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -1,14 +1,16 @@
 import sys
 input = sys.stdin.readline
-
-S1 = input().rstrip()
-S2 = input().rstrip()
-ans = 0
-dp = [[0 for _ in range(len(S1)+1)] for _ in range(len(S2)+1)]
-for i in range(len(S2)):
-    for j in range(len(S1)):
-        if S1[j] == S2[i]:
-            dp[i+1][j+1] = dp[i][j] + 1
-            ans = max(ans, dp[i+1][j+1])
-
-print(ans)
+def solve():
+    S1 = input().rstrip()
+    S2 = input().rstrip()
+    ans = 0
+    bef = [0] * (len(S1)+1)
+    for i in range(len(S2)):
+        cur = [0] * (len(S1)+1)
+        for j in range(len(S1)):
+            if S1[j] == S2[i]:
+                cur[j+1] = bef[j] + 1
+                ans = max(ans, cur[j+1])
+        bef = cur
+    print(ans)
+solve()
