f83fb69259869513eca9b4a7f30549cb237794b9
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-03-18 01:23:40 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 172 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,38 +1,41 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-
-steps = ((0, 0), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1))
-diag = (2, 4, 6, 8)
-
-clouds = [(N-2, 0), (N-2, 1), (N-1, 0), (N-1, 1)]
-
-for _ in range(M):
-    d, s = map(int, input().split())
-    rained = set()
-    while clouds:
-        x, y = clouds.pop()
-        dx, dy = steps[d]
-        nx = (x+dx*s) % N
-        ny = (y+dy*s) % N
-        board[nx][ny] += 1
-        rained.add((nx, ny))
-
-    for x, y in rained:
-        for d in diag:
+def solve():
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    
+    steps = ((0, 0), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1))
+    diag = (2, 4, 6, 8)
+    
+    clouds = [(N-2, 0), (N-2, 1), (N-1, 0), (N-1, 1)]
+    
+    for _ in range(M):
+        d, s = map(int, input().split())
+        rained = set()
+        while clouds:
+            x, y = clouds.pop()
             dx, dy = steps[d]
-            nx, ny = x+dx, y+dy
-            if nx < 0 or nx >= N or ny < 0 or ny >= N:
-                continue
-            if board[nx][ny] != 0:
-                board[x][y] += 1
+            nx = (x+dx*s) % N
+            ny = (y+dy*s) % N
+            board[nx][ny] += 1
+            rained.add((nx, ny))
+    
+        for x, y in rained:
+            for d in diag:
+                dx, dy = steps[d]
+                nx, ny = x+dx, y+dy
+                if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                    continue
+                if board[nx][ny] != 0:
+                    board[x][y] += 1
+    
+        for x in range(N):
+            for y in range(N):
+                if board[x][y] >= 2 and (x, y) not in rained:
+                    clouds.append((x, y))
+                    board[x][y] -= 2
+    
+    print(sum(map(sum, board)))
 
-    for x in range(N):
-        for y in range(N):
-            if board[x][y] >= 2 and (x, y) not in rained:
-                clouds.append((x, y))
-                board[x][y] -= 2
-
-print(sum(map(sum, board)))
+solve()
