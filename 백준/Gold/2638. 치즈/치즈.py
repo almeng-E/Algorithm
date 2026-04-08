86d77eed50fcefa7bf304edbd5b803ebf6cbb9a9
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-08 12:26:49 +0900

    [Gold III] Title: 치즈, Time: 300 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,32 +1,34 @@
 import sys
 input = sys.stdin.readline
-
 from collections import deque
 
-N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-cheese = sum(map(sum, board))
-ans = 0
-delta = ((0, 1), (1, 0), (0, -1), (-1, 0))
-
-while cheese:
-    ans += 1
-    v = [[0] * M for _ in range(N)]
-    q = deque()
-    q.append((0, 0))
-    v[0][0] = 2
-    while q:
-        x, y = q.popleft()
-        for dx, dy in delta:
-            nx, ny = x+dx, y+dy
-            if nx < 0 or nx >= N or ny < 0 or ny >= M or v[nx][ny] >= 2:
-                continue
-            if board[nx][ny] == 0:
-                v[nx][ny] = 2
-                q.append((nx, ny))
-            else:
-                v[nx][ny] += 1
-                if v[nx][ny] == 2:
-                    board[nx][ny] = 0
-                    cheese -= 1
-print(ans)
+def sol():
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    cheese = sum(map(sum, board))
+    ans = 0
+    delta = ((0, 1), (1, 0), (0, -1), (-1, 0))
+    
+    while cheese:
+        ans += 1
+        v = [[0] * M for _ in range(N)]
+        q = deque()
+        q.append((0, 0))
+        v[0][0] = 2
+        while q:
+            x, y = q.popleft()
+            for dx, dy in delta:
+                nx, ny = x+dx, y+dy
+                if nx < 0 or nx >= N or ny < 0 or ny >= M or v[nx][ny] >= 2:
+                    continue
+                if board[nx][ny] == 0:
+                    v[nx][ny] = 2
+                    q.append((nx, ny))
+                else:
+                    v[nx][ny] += 1
+                    if v[nx][ny] == 2:
+                        board[nx][ny] = 0
+                        cheese -= 1
+    print(ans)
+    
+sol()
