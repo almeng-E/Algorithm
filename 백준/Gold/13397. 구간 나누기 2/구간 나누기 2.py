127757040c6c89ed3cadbe5d95a697e3d3389027
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-12 13:59:30 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 48 ms, Memory: 33564 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.py
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.py
@@ -1,32 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-
-N, M = map(int, input().split())
-arr = list(map(int, input().split()))
-
-lo = 0
-hi = max(arr)-min(arr)
-ans = 0
-
-while lo <= hi:
-    mid = (lo+hi) >> 1
-
-    cnt = 1
-    cur_min = arr[0]
-    cur_max = arr[0]
-    for i in range(1, N):
-        cur_min = min(cur_min, arr[i])
-        cur_max = max(cur_max, arr[i])
-        if cur_max - cur_min > mid:
-            cnt += 1
-            cur_min = arr[i]
-            cur_max = arr[i]
-
-    if cnt <= M:
-        ans = mid
-        hi = mid - 1
-    else:
-        lo = mid + 1
-
-print(ans)
+def solve():
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+    
+    lo = 0
+    hi = max(arr)-min(arr)
+    ans = 0
+    
+    while lo <= hi:
+        mid = (lo+hi) >> 1
+    
+        cnt = 1
+        cur_min = arr[0]
+        cur_max = arr[0]
+        for i in range(1, N):
+            cur_min = min(cur_min, arr[i])
+            cur_max = max(cur_max, arr[i])
+            if cur_max - cur_min > mid:
+                cnt += 1
+                cur_min = arr[i]
+                cur_max = arr[i]
+    
+        if cnt <= M:
+            ans = mid
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    
+    print(ans)
+solve()
