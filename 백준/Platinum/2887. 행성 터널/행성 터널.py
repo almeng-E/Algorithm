0a8a91285768189063a050fe5e92ddc2ac2033ce
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-17 10:47:54 +0900

    [Platinum V] Title: 행성 터널, Time: 1140 ms, Memory: 102108 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -1,56 +1,58 @@
 import sys
 input = sys.stdin.readline
 
-def get_edge(axis):
-    axis.sort()
-    for i in range(N-1):
-        edges.append((abs(axis[i+1][0] - axis[i][0]), axis[i+1][1], axis[i][1]))
-
-
-def find(x):
-    if p[x] != x:
-        p[x] = find(p[x])
-    return p[x]
-
-
-def union(a, b):
-    rt_a = find(a)
-    rt_b = find(b)
-    if rt_a != rt_b:
-        p[rt_b] = p[rt_a]
-        return 1
-    else:
-        return 0
-
-
-N = int(input())
-
-X = []
-Y = []
-Z = []
-for i in range(N):
-    pos = map(int, input().split())
-    X.append((next(pos), i))
-    Y.append((next(pos), i))
-    Z.append((next(pos), i))
-
-edges = []
-get_edge(X)
-get_edge(Y)
-get_edge(Z)
-
-edges.sort()
-
-p = [i for i in range(N)]
-cnt = 0
-ans = 0
-
-for cost, a, b in edges:
-    if union(a, b):
-        ans += cost
-        cnt += 1
-
-    if cnt == N-1:
-        break
-
-print(ans)
+def solve():
+    def get_edge(axis):
+        axis.sort()
+        for i in range(N-1):
+            edges.append((abs(axis[i+1][0] - axis[i][0]), axis[i+1][1], axis[i][1]))
+    
+    
+    def find(x):
+        if p[x] != x:
+            p[x] = find(p[x])
+        return p[x]
+    
+    
+    def union(a, b):
+        rt_a = find(a)
+        rt_b = find(b)
+        if rt_a != rt_b:
+            p[rt_b] = p[rt_a]
+            return 1
+        else:
+            return 0
+    
+    
+    N = int(input())
+    
+    X = []
+    Y = []
+    Z = []
+    for i in range(N):
+        pos = map(int, input().split())
+        X.append((next(pos), i))
+        Y.append((next(pos), i))
+        Z.append((next(pos), i))
+    
+    edges = []
+    get_edge(X)
+    get_edge(Y)
+    get_edge(Z)
+    
+    edges.sort()
+    
+    p = [i for i in range(N)]
+    cnt = 0
+    ans = 0
+    
+    for cost, a, b in edges:
+        if union(a, b):
+            ans += cost
+            cnt += 1
+    
+        if cnt == N-1:
+            break
+    
+    print(ans)
+solve()
