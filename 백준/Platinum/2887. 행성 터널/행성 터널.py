d224622fea72a5be53e0b8c7f8b1a13507eba414
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-17 10:49:19 +0900

    [Platinum V] Title: 행성 터널, Time: 1224 ms, Memory: 102888 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -6,26 +6,29 @@ def solve():
         axis.sort()
         for i in range(N-1):
             edges.append((abs(axis[i+1][0] - axis[i][0]), axis[i+1][1], axis[i][1]))
-    
-    
+
+
     def find(x):
         if p[x] != x:
             p[x] = find(p[x])
         return p[x]
-    
-    
+
+
     def union(a, b):
         rt_a = find(a)
         rt_b = find(b)
         if rt_a != rt_b:
-            p[rt_b] = p[rt_a]
+            if sz[rt_b] > sz[rt_a]:
+                p[rt_a] = rt_b
+            else:
+                p[rt_b] = p[rt_a]
             return 1
         else:
             return 0
-    
-    
+
+
     N = int(input())
-    
+
     X = []
     Y = []
     Z = []
@@ -34,25 +37,26 @@ def solve():
         X.append((next(pos), i))
         Y.append((next(pos), i))
         Z.append((next(pos), i))
-    
+
     edges = []
     get_edge(X)
     get_edge(Y)
     get_edge(Z)
-    
+
     edges.sort()
-    
+
     p = [i for i in range(N)]
+    sz = [1 for _ in range(N)]
     cnt = 0
     ans = 0
-    
+
     for cost, a, b in edges:
         if union(a, b):
             ans += cost
             cnt += 1
-    
+
         if cnt == N-1:
             break
-    
+
     print(ans)
 solve()
