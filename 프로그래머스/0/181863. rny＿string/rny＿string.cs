ad17c34e12b299c44648876242422acc21f1258a
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-22 18:29:43 +0900

    [level 0] Title: rny_string, Time: 0.45 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public string solution(string rny_string) {
+        string answer = rny_string.Replace("m", "rn");
+        return answer;
+    }
+}
