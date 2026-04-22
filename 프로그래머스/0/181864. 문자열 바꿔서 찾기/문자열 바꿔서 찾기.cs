195a76c4cb499c255130a296aeb8e9b74569778d
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-22 18:58:01 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.80 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cs
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cs
@@ -4,21 +4,9 @@ using System.Linq;
 
 public class Solution {
     public int solution(string myString, string pat) {
-        int N = myString.Length;
-        List<char> li = new List<char>();
-        for (int i=0; i<N; ++i) {
-            if (myString[i] == 'A') {
-                li.Add('B');
-            } else {
-                li.Add('A');
-            }
-        }
-        int M = pat.Length;
-        for (int i=0; i<N-M+1; ++i) {
-            if (pat == new string(li.GetRange(i, M).ToArray())){
-                return 1;
-            }
-        }
-        return 0;
+        string conv = myString.Replace("A", "b").Replace("B", "a");
+        
+        return conv.Contains(pat.ToLower()) ? 1: 0;
+        
     }
 }
