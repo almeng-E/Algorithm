6a253f6d75cbb4620ccc78b193708933ea53e69b
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-22 22:30:56 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 0.35 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) {
+        int N = queries.GetLength(0);
+        for (int i=0; i<N; ++i) {
+            int s = queries[i,0];
+            int e = queries[i,1];
+            for (int j=s; j<=e; ++j) {
+                arr[j]++;
+            }
+        }
+        
+        
+        
+        return arr;
+    }
+}
