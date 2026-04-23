0fc3a9113fea9aba6475f7ecba6bd133b905998e
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-23 23:34:15 +0900

    [level 3] Title: 입국심사, Time: 55.64 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Solution {
+    long ANS;
+    int[] TIME;
+    int N;
+    
+    public bool check(long mid) {
+        long cnt = 0;
+        for (int i=0; i<TIME.Length; ++i) {
+            cnt += mid/TIME[i];
+        }
+        if (cnt>=N) return true;
+        return false;
+    }
+    
+    
+    public long solution(int n, int[] times) {
+        ANS = 0;
+        TIME = times;
+        N = n;
+        
+        long l = 0;
+        long r = (long)Math.Pow(10, 18);
+        while (l <= r) {
+            long mid = (l+r) >> 1;
+            if (check(mid)) {
+                // 가능
+                ANS = mid;
+                r = mid-1;
+            }    
+            else {
+                // 불가능
+                l = mid+1;
+            }
+        }
+        
+        return ANS;
+    }
+}
