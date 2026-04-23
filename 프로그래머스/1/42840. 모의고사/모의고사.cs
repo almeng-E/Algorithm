dcaa81b1aa433c3ec844bb7873836783cd7bc2e1
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-23 19:01:39 +0900

    [level 1] Title: 모의고사, Time: 1.35 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cs
+++ 프로그래머스/1/42840. 모의고사/모의고사.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
-    public int[] solution(int[] answers) {
+    int[] answers;
+    
+    public int[] solution(int[] inp) {
+        answers = inp;
         var ret = new List<int>();        
         int[] a1 = new int[]{1, 2, 3, 4, 5};
         int[] a2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
         int[] a3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         var scores = new List<int>();
         scores.Add(0);
-        scores.Add(calc(a1, answers));
-        scores.Add(calc(a2, answers));
-        scores.Add(calc(a3, answers));
+        scores.Add(calc(a1));
+        scores.Add(calc(a2));
+        scores.Add(calc(a3));
         
         int maxScore = 0;
         for (int i=1; i<4; ++i) {
@@ -31,7 +34,7 @@ public class Solution {
         return answer;
     }
 
-    public int calc(int[] person, int[] answers) {
+    public int calc(int[] person) {
         int N = answers.Length;
         int M = person.Length;
         int ret = 0;
