4f4b35fb1c4aaf8d970248f7a246f2bca187e61b
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-24 19:29:03 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cs
+++ 프로그래머스/3/43162. 네트워크/네트워크.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
+    bool[] v;
+    int N;
     public int solution(int n, int[,] computers) {
         int answer = 0;
-        bool[] v = new bool[n];
-        
-        for (int i=0; i<n; ++i) {
-            if (v[i]) continue;
-            v[i] = true;
-            answer++;
-            var q = new Queue<int>();
-            q.Enqueue(i);
-            while (q.Count != 0) {
-                int cur = q.Dequeue();
-                for (int nxt=0; nxt<n; ++nxt) {
-                    if (v[nxt]) continue;
-                    if (computers[cur,nxt] == 1) {
-                        v[nxt] = true;
-                        q.Enqueue(nxt);
-                    }
-                }
+        v = new bool[n];
+        N = n;
+        for (int i=0; i<N; ++i) {
+            if (!v[i]) {
+                answer++;
+                v[i] = true;
+                dfs(i, computers);
             }
         }
         
+        
         return answer;
+    }
+    public void dfs(int cur, int[,] graph){
+        for (int nxt=0; nxt<N; ++nxt) {
+            if (graph[cur,nxt] == 0 || v[nxt]) continue;
+            v[nxt] = true;
+            dfs(nxt, graph);
+        }
     }
 }
