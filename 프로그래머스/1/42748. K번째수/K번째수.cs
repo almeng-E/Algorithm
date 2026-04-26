ba0f1d36610e41cb4811d8e58189e3de0ad7fdff
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-26 13:07:20 +0900

    [level 1] Title: K번째수, Time: 2.82 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cs
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -4,18 +4,17 @@ using System.Linq;
 
 public class Solution {
     public int[] solution(int[] array, int[,] commands) {
-        
-        int cLen = commands.GetLength(0);
-        int[] answer = new int[cLen];
-        for (int qi=0; qi<cLen; ++qi) {
-            int i = commands[qi,0] - 1;
-            int j = commands[qi,1] - 1;
-            int k = commands[qi,2] - 1;
-            
-            var tmp = array.Skip(i).Take(j-i+1).ToList();
-            tmp.Sort();
-            answer[qi] = tmp[k];
+        int[] answer = new int[commands.GetLength(0)];
+        for (int ci=0; ci<commands.GetLength(0); ++ci) {
+            int i=commands[ci,0] -1 , j=commands[ci,1] -1, k=commands[ci,2] -1;
+            var li = new List<int>();
+            for (int x=i; x<=j; ++x) {
+                li.Add(array[x]);
+            }
+            li.Sort();
+            answer[ci] = li[k];            
         }
+        
         return answer;
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 31.5 MB, 시간: 4.08 ms
+메모리: 31.2 MB, 시간: 2.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 23일 14:03:45
+2026년 04월 26일 13:07:19
 
 ### 문제 설명
 
