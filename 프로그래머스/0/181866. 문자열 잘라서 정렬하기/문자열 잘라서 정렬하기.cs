9817abebd7b3708e9b1853f58fabf081b60a8fac
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-26 12:32:03 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 153.98 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public string[] solution(string myString) {
+        string[] answer = myString.Split("x").Where(s => s!="").OrderBy(s=>s).ToArray();
+
+        
+        return answer;
+    }
+}
