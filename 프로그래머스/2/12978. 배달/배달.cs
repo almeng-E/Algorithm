2e87b8392d590514e6ca2fe8dbc35c0b1168817c
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-04-26 12:18:31 +0900

    [level 2] Title: 배달, Time: 5.74 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cs
+++ 프로그래머스/2/12978. 배달/배달.cs
@@ -2,10 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
 class Solution {
-    public int solution(int N, int[,] road, int K) {
-        // G[cur] = [(nxt, w), ...]
+    public int solution(int N, int[,] road, int K)     {
         var G = new List<(int nxt, int w)>[N+1];
         for (int i=0; i<=N; ++i) {
             G[i] = new List<(int nxt, int w)>();
@@ -15,23 +13,24 @@ class Solution {
             G[a].Add((b, c));
             G[b].Add((a, c));
         }
-        
         int INF = (int)1e9;
-        // PriorityQueue 대신 SortedSet...
-        var PQ = new SortedSet<(int d, int cur)>();
         var dist = new int[N+1];
         for (int i=0; i<=N; ++i) {
             dist[i] = INF;
         }
-        PQ.Add((0, 1));
         dist[1] = 0;
+        var PQ = new SortedSet<(int d, int cur)>();
+        PQ.Add((0, 1));
+        
         while (PQ.Count > 0) {
             var top = PQ.Min;
+            int d = top.d, cur=top.cur;
             PQ.Remove(top);
-            int d = top.d, cur = top.cur; 
+            
             if (dist[cur] < d) continue;
-            foreach (var nei in G[cur]) {
-                int nxt = nei.nxt, w = nei.w;
+            
+            foreach (var item in G[cur]) {
+                int nxt=item.nxt, w=item.w;
                 int nd = d+w;
                 if (dist[nxt] > nd) {
                     dist[nxt] = nd;
@@ -39,11 +38,11 @@ class Solution {
                 }
             }
         }
+        
         int answer = 0;
         for (int i=1; i<=N; ++i) {
             if (dist[i] <= K) answer++;
         }
-        
         return answer;
     }
 }
