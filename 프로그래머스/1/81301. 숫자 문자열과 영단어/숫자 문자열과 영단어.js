cfba2a2746b7748d721f4dd51d6e547295ee62e5
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-07-20 19:52:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.17 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,24 @@
+function solution(s) {
+    let answer = 0;
+    
+    const arr = new Map([
+        ['zero', '0'],
+        ['one', '1'],
+        ['two', '2'],
+        ['three', '3'],
+        ['four', '4'],
+        ['five', '5'],
+        ['six', '6'],
+        ['seven', '7'],
+        ['eight', '8'],
+        ['nine', '9'],
+    ])
+    
+    for (let [k, v] of arr) {
+        s = s.replaceAll(k, v);
+    }
+    
+    
+    
+    return Number(s);
+}
