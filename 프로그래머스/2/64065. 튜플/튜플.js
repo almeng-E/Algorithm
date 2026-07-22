523dccfd504543f71466b32bb3609d8c6001fe58
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-07-22 10:30:31 +0900

    [level 2] Title: 튜플, Time: 22.68 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -5,15 +5,15 @@ function solution(s) {
     for (let i=0; i<arr.length; ++i) {
         arr[i] = new Set(arr[i].split(','));
     }
-    arr.sort((a, b) => a.size - b.size);
-
-    for (let i=arr.length-1; i>0; --i) {
-        arr[i] = arr[i].difference(arr[i-1]);
-    }
-    for (let i=0; i<arr.length; ++i) {
-        answer.push(Number(arr[i].values().next().value));
-    }
+    arr.push(new Set());
+    arr.sort((a, b) => {
+        if (a.size < b.size) return -1;
+        else return 1;
+    })
     
-
+    for (let i=1; i<arr.length; ++i) {
+        const [x] = arr[i].difference(arr[i-1])
+        answer.push(Number(x));
+    }
     return answer;
 }
