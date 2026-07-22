d0c17975d48230a12192a49449bb914d413d5b59
Choi Wonmin <105558602+almeng-E@users.noreply.github.com>
2026-07-22 10:15:28 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -2,22 +2,26 @@ function solution(priorities, location) {
     let answer = 0;
     
     const N = priorities.length;
-    const P = [...priorities].sort((a, b) => b-a);
-    let P_idx = 0;
+    const order = [...priorities].sort((a, b) => b-a);
     
-    const used = new Array(N).fill(false);
-    let cur_idx = 0;
+    let oIdx = 0;
+    let pIdx = 0;
     
-    while (P_idx < N) {
-        if (!used[cur_idx] && (P[P_idx] === priorities[cur_idx])) {
-            P_idx++;
-            used[cur_idx] = true;
-            if (cur_idx === location) return P_idx;
+    const res = [];
+    while (oIdx < N) {
+        if (priorities[pIdx] === order[oIdx]) {
+            oIdx++;
+            res.push(pIdx);
         }
-        cur_idx++;
-        cur_idx%=N;
+        pIdx++;
+        pIdx %= N;
     }
     
-    
+    for (let i=0; i<N; ++i) {
+        if (res[i] === location) {
+            answer = i+1;
+            break;
+        }
+    }
     return answer;
 }
